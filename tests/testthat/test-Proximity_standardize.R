d55eb2096d486178964a0ea8abd55d08bf4b5e32
ncbolano <142913812+ncbolano@users.noreply.github.com>
2023-11-11 16:21:41 -0600

    Bug fixed tests 3 and 4 , which had incorrect variable names being called

--- tests/testthat/test-Proximity_standardize.R
+++ tests/testthat/test-Proximity_standardize.R
@@ -1,5 +1,7 @@
 test_that("Proximity_standardize standardizes each row in the proximity matrix correctly", {
+
   # Test (1-3) (2x2,3x3,4x4 matrices that should still remain symmetric after standardization)
+
   proximity1 = matrix(c(0,1,1,0), nrow = 2, byrow = TRUE)
   resulting_matrix1 = Proximity_standardize(proximity1)
   expected_matrix1 = matrix(c(0,1,1,0), nrow = 2, byrow = TRUE)
@@ -7,14 +9,16 @@ test_that("Proximity_standardize standardizes each row in the proximity matrix c
 
   proximity2 = matrix(c(0,1,1,1,0,1,1,1,0), nrow = 3, byrow = TRUE)
   resulting_matrix2 = Proximity_standardize(proximity2)
-  expected_matrix2 = matrix(c(0,1/2,1/2,1/2,0,1/2,1/2,1/2,0),nrow = 3, byrow = TRUE)
+  expected_matrix2 = matrix(c(0,1/2,1/2,1/2,0,1/2,1/2,1/2,0), nrow = 3, byrow = TRUE)
   expect_equal(resulting_matrix2, expected_matrix2)
 
   proximity3 = matrix(c(0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0), nrow = 4, byrow = TRUE)
-  resulting_matrix2 = Proximity_standardize(proximity3)
+  resulting_matrix3 = Proximity_standardize(proximity3)
   expected_matrix3 = matrix(c(0,1/2,0,1/2,1/2,0,1/2,0,0,1/2,0,1/2,1/2,0,1/2,0), nrow = 4,
                             byrow = TRUE)
   expect_equal(resulting_matrix3, expected_matrix3)
+
+
 
   # Test (4) (Matrix that will not remain symmetric after standardization)
   proximity4 = matrix(c(0,1,0,1,0,
@@ -22,8 +26,8 @@ test_that("Proximity_standardize standardizes each row in the proximity matrix c
                         0,1,0,0,1,
                         1,1,0,0,1,
                         0,1,1,1,0), nrow = 5, byrow = TRUE)
-  resulting_matrix2 = Proximity_standardize(proximity3)
-  expected_matrix3 = matrix(c(0,1/2,0,1/2,0,1/4,0,1/4,1/4,1/4,0,1/2,0,0,1/2,
+  resulting_matrix4 = Proximity_standardize(proximity4)
+  expected_matrix4 = matrix(c(0,1/2,0,1/2,0,1/4,0,1/4,1/4,1/4,0,1/2,0,0,1/2,
                               1/3,1/3,0,0,1/3,0,1/3,1/3,1/3,0),
                                nrow = 5, byrow = TRUE)
   expect_equal(resulting_matrix4, expected_matrix4)
