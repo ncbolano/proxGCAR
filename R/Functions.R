96c6df59fa8da552f8ee17f9236a254b4296cb04
ncbolano <142913812+ncbolano@users.noreply.github.com>
2023-11-10 22:59:38 -0600

    Creation of subsection of proximity_check testing that nrow(proximity) = ncol(proximity)

--- R/Functions.R
+++ R/Functions.R
@@ -4,10 +4,18 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-Proximity_setup = function(proximity) {
+Proximity_check = function(proximity) {
+  if(nrow(proximity) != ncol(proximity)) {
+    stop("Your proximity matrix must be a square matrix.
+    For further aid towards construction of the proximity matrix, refer to the
+    package description.")
+  }
   if (any(proximity != 0 & proximity != 1)) {
     stop("Your proximity matrix contains a value that is not ( 0 or 1 ).
-       For further aid towards construction of the proximity matrix, refer to the
-       package description.")
+    For further aid towards construction of the proximity matrix, refer to the
+    package description.")
+  }
+  if (any((rowSums(proximity) = 0) | (colSums(proximity) = 0))) {
+
   }
 }
