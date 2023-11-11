2deeeabc1c68320818ca7ba73de6a454d3a07bd5
ncbolano <142913812+ncbolano@users.noreply.github.com>
2023-11-11 16:17:51 -0600

    Added two extra test (3,4) and finished all the tests (untested if this is working properly)

--- tests/testthat/test-Proximity_standardize.R
+++ tests/testthat/test-Proximity_standardize.R
@@ -1,11 +1,30 @@
 test_that("Proximity_standardize standardizes each row in the proximity matrix correctly", {
-  # Test 1 (2x2)
-  proximity1 = matrix(c(0,1,1,0), nrow = 2)
+  # Test (1-3) (2x2,3x3,4x4 matrices that should still remain symmetric after standardization)
+  proximity1 = matrix(c(0,1,1,0), nrow = 2, byrow = TRUE)
   resulting_matrix1 = Proximity_standardize(proximity1)
-  expected_matrix1 = matrix(c(0,1,1,0), nrow = 2)
+  expected_matrix1 = matrix(c(0,1,1,0), nrow = 2, byrow = TRUE)
+  expect_equal(resulting_matrix1, expected_matrix1)
 
-  # Test 2 (3x3)
-  proximity2 = matrix(c(0,1,1,1,0,1,1,1,0), nrow = 3)
+  proximity2 = matrix(c(0,1,1,1,0,1,1,1,0), nrow = 3, byrow = TRUE)
   resulting_matrix2 = Proximity_standardize(proximity2)
-  expected_matrix2 = matrix(0,1/2,1/2,1/2,0,1/2,1/2,1/2,0)
+  expected_matrix2 = matrix(c(0,1/2,1/2,1/2,0,1/2,1/2,1/2,0),nrow = 3, byrow = TRUE)
+  expect_equal(resulting_matrix2, expected_matrix2)
+
+  proximity3 = matrix(c(0,1,0,1,1,0,1,0,0,1,0,1,1,0,1,0), nrow = 4, byrow = TRUE)
+  resulting_matrix2 = Proximity_standardize(proximity3)
+  expected_matrix3 = matrix(c(0,1/2,0,1/2,1/2,0,1/2,0,0,1/2,0,1/2,1/2,0,1/2,0), nrow = 4,
+                            byrow = TRUE)
+  expect_equal(resulting_matrix3, expected_matrix3)
+
+  # Test (4) (Matrix that will not remain symmetric after standardization)
+  proximity4 = matrix(c(0,1,0,1,0,
+                        1,0,1,1,1,
+                        0,1,0,0,1,
+                        1,1,0,0,1,
+                        0,1,1,1,0), nrow = 5, byrow = TRUE)
+  resulting_matrix2 = Proximity_standardize(proximity3)
+  expected_matrix3 = matrix(c(0,1/2,0,1/2,0,1/4,0,1/4,1/4,1/4,0,1/2,0,0,1/2,
+                              1/3,1/3,0,0,1/3,0,1/3,1/3,1/3,0),
+                               nrow = 5, byrow = TRUE)
+  expect_equal(resulting_matrix4, expected_matrix4)
 })
