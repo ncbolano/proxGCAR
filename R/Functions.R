4b81be7cee42d671e7805acebf0b26fe5457c451
ncbolano <142913812+ncbolano@users.noreply.github.com>
2023-11-10 23:31:50 -0600

    Added test to ensure all diagonal entries are 0 in proximity

--- R/Functions.R
+++ R/Functions.R
@@ -5,17 +5,22 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 Proximity_check = function(proximity) {
+  # Checking matrix is square
   if(nrow(proximity) != ncol(proximity)) {
     stop("Your proximity matrix must be a square matrix.
     For further aid towards construction of the proximity matrix, refer to the
     package description.")
   }
+  # Checking matrix has all 0 values for it's diagonals
+  if(any(diag(proximity) != 0)) {
+    stop("Your proximity matrix should have all diagonal entries equal to 0. For
+    further aid towards construction of the proximity matrix, refer to the
+    package description.")
+  }
+  # Checking matrix has all values 0 or 1
   if (any(proximity != 0 & proximity != 1)) {
     stop("Your proximity matrix contains a value that is not ( 0 or 1 ).
     For further aid towards construction of the proximity matrix, refer to the
     package description.")
-  }
-  if (any((rowSums(proximity) = 0) | (colSums(proximity) = 0))) {
-
   }
 }
