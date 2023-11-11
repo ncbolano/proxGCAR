1de539e9094d6f28ecab2fd3952e179e72bd7214
ncbolano <142913812+ncbolano@users.noreply.github.com>
2023-11-10 23:51:02 -0600

    Added parts to Proximity_check to make sure i) all values are 0 or 1 , ii) matrix is symmetric iii) All rowSums > 0

--- R/Functions.R
+++ R/Functions.R
@@ -5,22 +5,35 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 Proximity_check = function(proximity) {
-  # Checking matrix is square
+  # Checking matrix is square (2)
   if(nrow(proximity) != ncol(proximity)) {
     stop("Your proximity matrix must be a square matrix.
     For further aid towards construction of the proximity matrix, refer to the
     package description.")
   }
-  # Checking matrix has all 0 values for it's diagonals
-  if(any(diag(proximity) != 0)) {
-    stop("Your proximity matrix should have all diagonal entries equal to 0. For
-    further aid towards construction of the proximity matrix, refer to the
-    package description.")
-  }
-  # Checking matrix has all values 0 or 1
+  # Checking matrix has all values 0 or 1 (3)
   if (any(proximity != 0 & proximity != 1)) {
     stop("Your proximity matrix contains a value that is not ( 0 or 1 ).
     For further aid towards construction of the proximity matrix, refer to the
     package description.")
+  }
+  # Checking matrix has all 0 values for it's diagonals (4)
+  if(any(diag(proximity) != 0)) {
+    stop("Your proximity matrix should have all diagonal entries equal to 0. For
+    further aid towards construction of the proximity matrix, refer to the
+    package description.")
+  # Checking matrix is symmetric (5)
+  if(!isSymmetric(proximity)) {
+    stop("Your proximity matrix should be symmetric. For further aid towards construction of the proximity matrix,
+    refer to the package description.")
+  }
+  # Checking matrix has all rowSums > 0. (No check for colSums since symmetric (6)
+  if(!all(rowSums(proximity) > 0)){
+    stop("Your proximity matrix should have at least one conditional dependance
+    for each row. For further aid towards construction of the proximity matrix,
+    refer to the package description.")
+  }
+
+
   }
 }
