be0886f5356fca72c653177c5222bc4478dfa1eb
ncbolano <142913812+ncbolano@users.noreply.github.com>
2023-11-11 16:37:42 -0600

    Added a fifth test that is expected to fail because of incorrect expected matrix. All tests working.

--- tests/testthat/test-Proximity_standardize.R
+++ tests/testthat/test-Proximity_standardize.R
@@ -1,7 +1,6 @@
 test_that("Proximity_standardize standardizes each row in the proximity matrix correctly", {
 
   # Test (1-3) (2x2,3x3,4x4 matrices that should still remain symmetric after standardization)
-
   proximity1 = matrix(c(0,1,1,0), nrow = 2, byrow = TRUE)
   resulting_matrix1 = Proximity_standardize(proximity1)
   expected_matrix1 = matrix(c(0,1,1,0), nrow = 2, byrow = TRUE)
@@ -18,17 +17,13 @@ test_that("Proximity_standardize standardizes each row in the proximity matrix c
                             byrow = TRUE)
   expect_equal(resulting_matrix3, expected_matrix3)
 
-
-
   # Test (4) (Matrix that will not remain symmetric after standardization)
-  proximity4 = matrix(c(0,1,0,1,0,
-                        1,0,1,1,1,
-                        0,1,0,0,1,
-                        1,1,0,0,1,
-                        0,1,1,1,0), nrow = 5, byrow = TRUE)
+  proximity4 = matrix(c(0,1,0,1,0,1,0,1,1,1,0,1,0,0,1,1,1,0,0,1,0,1,1,1,0),
+                      nrow = 5, byrow = TRUE)
   resulting_matrix4 = Proximity_standardize(proximity4)
   expected_matrix4 = matrix(c(0,1/2,0,1/2,0,1/4,0,1/4,1/4,1/4,0,1/2,0,0,1/2,
                               1/3,1/3,0,0,1/3,0,1/3,1/3,1/3,0),
                                nrow = 5, byrow = TRUE)
   expect_equal(resulting_matrix4, expected_matrix4)
+
 })
