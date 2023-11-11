42824d1a53b552f5129e3522eb333777b026f71d
ncbolano <142913812+ncbolano@users.noreply.github.com>
2023-11-11 00:06:22 -0600

    Renumbered sections of Proximity_check according to the order of tests in testing section.

--- R/Functions.R
+++ R/Functions.R
@@ -5,30 +5,30 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 Proximity_check = function(proximity) {
-  # Checking matrix is square (2)
+  # Checking matrix is square (1)
   if(nrow(proximity) != ncol(proximity)) {
     stop("Your proximity matrix must be a square matrix.
     For further aid towards construction of the proximity matrix, refer to the
     package description.")
   }
-  # Checking matrix has all values 0 or 1 (3)
+  # Checking matrix has all values 0 or 1 (2)
   if (any(proximity != 0 & proximity != 1)) {
     stop("Your proximity matrix contains a value that is not ( 0 or 1 ).
     For further aid towards construction of the proximity matrix, refer to the
     package description.")
   }
-  # Checking matrix has all 0 values for it's diagonals (4)
+  # Checking matrix has all 0 values for it's diagonals (3)
   if(any(diag(proximity) != 0)) {
     stop("Your proximity matrix should have all diagonal entries equal to 0. For
     further aid towards construction of the proximity matrix, refer to the
     package description.")
   }
-  # Checking matrix is symmetric (5)
+  # Checking matrix is symmetric (4)
   if(!isSymmetric(proximity)) {
     stop("Your proximity matrix should be symmetric. For further aid towards
     construction of the proximity matrix, refer to the package description.")
   }
-  # Checking matrix has all rowSums > 0. (No check for colSums since symmetric (6)
+  # Checking matrix has all rowSums > 0. (No check for colSums since symmetric (5)
   if(!all(rowSums(proximity) > 0)){
     stop("Your proximity matrix should have at least one conditional dependance
     for each row. For further aid towards construction of the proximity matrix,
