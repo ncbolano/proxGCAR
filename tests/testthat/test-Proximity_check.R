38b2923b11025b04389b03c513b2d97a779b5a71
ncbolano <142913812+ncbolano@users.noreply.github.com>
2023-11-10 23:33:34 -0600

    Added subsequent test to check all diagonal values = 0 is working.

--- tests/testthat/test-Proximity_check.R
+++ tests/testthat/test-Proximity_check.R
@@ -10,4 +10,8 @@ test_that("Proximity_setup handles matrices correctly", {
   # Test 3 : (Given invalid matrix , some value not equal to (0 or 1))
   proximity3 = matrix(c(0,2,2,0), nrow = 2)
   expect_error(Proximity_check(proximity3))
+
+  # Test 4 : (Given invalid matrix , some diagonal value not equal to 0)
+  proximity4 = matrix(c(1,1,1,1), nrow = 2)
+  expect_error(Proximity_check(proximity4))
 })
