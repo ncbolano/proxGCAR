7fb050cbe74913c6ca960cd58774c38d229a43c1
ncbolano <142913812+ncbolano@users.noreply.github.com>
2023-11-10 23:27:12 -0600

    Added test that ensured ncol(proximity) = nrow(proximity)

--- tests/testthat/test-Proximity_check.R
+++ tests/testthat/test-Proximity_check.R
@@ -2,9 +2,12 @@ test_that("Proximity_setup handles matrices correctly", {
   # Test 1 : (Given valid matrix)
   proximity1 = matrix(c(0,1,1,0), nrow = 2)
   expect_silent(Proximity_check(proximity1))
-  # Test 3 : (Given invalid matrix with some value not equal to (0 or 1))
+
+  # Test 2 : (Given invalid matrix , nrow(proximity) != ncol(proximity))
+  proximity2 = matrix(c(0,1,1,1,0,1), nrow = 2)
+  expect_error(Proximity_check(proximity2))
+
+  # Test 3 : (Given invalid matrix , some value not equal to (0 or 1))
   proximity3 = matrix(c(0,2,2,0), nrow = 2)
   expect_error(Proximity_check(proximity3))
-
-
 })
