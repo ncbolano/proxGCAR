d1084bcccb5614976c83f06b3aed4405a8e22756
ncbolano <142913812+ncbolano@users.noreply.github.com>
2023-11-10 22:04:59 -0600

    Create check to ensure an unstandardized proximity matrix only has values of 0's and 1's

--- R/Functions.R
+++ R/Functions.R
@@ -5,4 +5,9 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 Proximity_setup = function(proximity) {
+  if (any(proximity != 0 & proximity != 1)) {
+    stop("Your proximity matrix contains a value that is not ( 0 or 1 ).
+       For further aid towards construction of the proximity matrix, refer to the
+       package description.")
+  }
 }
